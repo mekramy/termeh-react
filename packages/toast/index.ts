da744a98d0d3ba57284fa26b87ad5bb65890cd0d
mekramy <m.ajamekramy@gmail.com>
2026-09-08 15:05:11 +0330

    feat: minor export and style optimization

--- packages/toast/index.ts
+++ packages/toast/index.ts
@@ -1,6 +1,19 @@
 export * from "./components/BaseToast";
 export * from "./components/Simple";
+export * from "./components/ToastProvider";
 export * from "./hooks/useCreate";
 export * from "./hooks/useToast";
-export * from "./providers/ToastProvider";
-export * from "./types";
+export type {
+    ActionBarAnimation,
+    ActionHandler,
+    Animations,
+    Callback,
+    ClickHandler,
+    CloseHandler,
+    CloseMode,
+    Mode,
+    OpenHandler,
+    Options,
+    ProviderOptions,
+    State,
+} from "./types";
